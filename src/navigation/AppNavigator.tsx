acdda020ef8a513a750fb61f1370ad91544871cb
Robert Drez <139603713+OjDrez@users.noreply.github.com>
2025-12-03 14:57:34 +0800

    Add PeopleViewedMe screen to app navigation

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import HomeScreen from "../screens/Home/HomeScreen";
 import InboxScreen from "../screens/Inbox/InboxScreen";
 import MyMatchesScreen from "../screens/Matches/MyMatchesScreen";
+import PeopleViewedMeScreen from "../screens/ViewMe/PeopleViewedMeScreen";
 import ProfileViewScreen from "../screens/Profile/ProfileViewScreen";
 import { AppStackParamList } from "./NavigationTypes";
 
@@ -15,6 +16,10 @@ export default function AppNavigator() {
       <Stack.Screen name="HomeScreen" component={HomeScreen} />
       <Stack.Screen name="InboxScreen" component={InboxScreen} />
       <Stack.Screen name="MyMatchesScreen" component={MyMatchesScreen} />
+      <Stack.Screen
+        name="PeopleViewedMeScreen"
+        component={PeopleViewedMeScreen}
+      />
       <Stack.Screen name="ProfileViewScreen" component={ProfileViewScreen} />
     </Stack.Navigator>
   );
